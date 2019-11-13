915740900e1b98549d204867dfc8c043125164a9
fynnbe <thefynnbe@gmail.com>
2019-11-13 17:00:33 +0100

    use CUDA_VISIBLE_DEVICES env var (allows to use slurm)

--- lnet/__main__.py
+++ lnet/__main__.py
@@ -10,7 +10,7 @@ CONFIG = {
     "disable_existing_loggers": False,
     "formatters": {
         "default": {
-            "format": "%(asctime)s %(name)s %(levelname)s %(message)s", # .%(msecs)03d [%(processName)s/%(threadName)s]
+            "format": "%(asctime)s %(name)s %(levelname)s %(message)s",  # .%(msecs)03d [%(processName)s/%(threadName)s]
             "datefmt": "%H:%M:%S",
         }
     },
@@ -34,12 +34,22 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="lnet")
     parser.add_argument("experiment_config", type=Path)
-    parser.add_argument("CUDA_VISIBLE_DEVICES", type=str)
+    parser.add_argument("--cuda", metavar="CUDA_VISIBLE_DEVICES", type=str, nargs="?", const="0", default=None)
     parser.add_argument("--test", action="store_true")
 
     args = parser.parse_args()
     assert args.experiment_config.exists(), args.experiment_config.absolute()
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.CUDA_VISIBLE_DEVICES
+
+    cuda_arg = args.cuda
+    cuda_env = os.environ.get("CUDA_VISIBLE_DEVICES", None)
+    if cuda_env is None:
+        if cuda_arg is None:
+            raise ValueError("'CUDA_VISIBLE_DEVICES' not specified")
+        else:
+            os.environ["CUDA_VISIBLE_DEVICES"] = cuda_arg
+    elif cuda_arg is not None:
+        if cuda_env != cuda_arg:
+            raise ValueError("env and arg values for 'CUDA_VISIBLE_DEVICES' unequal!")
 
     from lnet.experiment import Experiment
 
