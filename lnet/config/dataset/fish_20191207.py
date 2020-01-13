fde34e3ae73dae4a31b289a630824dab384d5b13
fynnbe <thefynnbe@gmail.com>
2020-01-13 13:41:28 +0100

    add 201191207 fish

---
+++ lnet/config/dataset/fish_20191207.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+
+from lnet.config.dataset import NamedDatasetInfo
+
+
+fish1_20191208_0610_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.10.34"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0610_static",
+    length=None,
+)
+fish1_20191208_0618_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.18.09"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0618_static",
+    length=None,
+)
+fish1_20191208_0623_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.23.13"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0623_static",
+    length=None,
+)
+fish1_20191208_0625_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.25.02"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0625_static",
+    length=None,
+)
+fish1_20191208_0630_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.30.40"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0630_static",
+    length=None,
+)
+fish1_20191208_0635_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.35.52"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0635_static",
+    length=None,
+)
+fish1_20191208_0638_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.38.47"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0638_static",
+    length=None,
+)
+fish1_20191208_0641_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.41.39"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0641_static",
+    length=None,
+)
+fish1_20191208_0646_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.46.09"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0646_static",
+    length=None,
+)
+fish1_20191208_0649_static = NamedDatasetInfo(
+    Path(
+        "/g/kreshuk/LF_partially_restored/LenseLeNet_Microscope/20191207_StaticHeart/fish1/static/Heart_tightCrop/centeredPos_5steps_stepsize8/probablyStatic/2019-12-08_06.49.08"
+    ),
+    "stack_3_channel_0/TP_*/RC_rectified/Cam_Right_001_rectified.tif",
+    "stack_4_channel_1/TP_*/LC/Cam_Left_registered.tif",
+    "fish1_20191208_0649_static",
+    length=None,
+)
