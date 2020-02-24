d99df44a163b8e141441269d5b8a10bf7b600b0f
fynnbe <thefynnbe@gmail.com>
2020-02-24 10:12:36 +0100

    repair tensorboard script

--- scripts/tensorboard.py
+++ scripts/tensorboard.py
@@ -1,39 +1,68 @@
 import argparse
 import datetime
+import logging.config
 from pathlib import Path
 from subprocess import STDOUT, TimeoutExpired, check_output
 from time import sleep
 
 LOG_DIR = Path("/g/kreshuk/beuttenm/repos/lnet/logs")
 
+CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s %(name)s %(levelname)s %(message)s",  # .%(msecs)03d [%(processName)s/%(threadName)s]
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        }
+    },
+    "handlers": {
+        "default": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "default",
+        }
+    },
+    "loggers": {
+        "": {"handlers": ["default"], "level": "INFO", "propagate": True},
+    },
+}
+logging.config.dictConfig(CONFIG)
+logger = logging.getLogger(__name__)
+
 
 def run_tensorboard(*, logdir: str, start: datetime.time, stop: datetime.time):
     now = datetime.datetime.now()
     start = datetime.datetime.combine(now.date(), start)
     if start < now:
-        start + datetime.timedelta(days=1)
+        start += datetime.timedelta(days=1)
 
     stop = datetime.datetime.combine(now.date(), stop)
     if stop < now:
-        stop + datetime.timedelta(days=1)
+        stop += datetime.timedelta(days=1)
 
+    assert start > now, start
+    assert stop > now, stop
     while True:
         if start.weekday() > 5:
             start + datetime.timedelta(days=1)
-        elif start < stop:
-            print("running until", stop)
+
+        duration = (min(start, stop) - datetime.datetime.now()).total_seconds()
+        if stop < start:
+            logger.info("running for %ds until %s", duration, stop)
             try:
                 check_output(
-                    ["tensorboard", "--logdir", str(logdir)], stderr=STDOUT, timeout=(stop - start).total_seconds()
+                    ["tensorboard", "--logdir", str(logdir), "--port", "5158"], stderr=STDOUT, timeout=duration
                 )
             except TimeoutExpired:
                 pass
 
-            start + datetime.timedelta(days=1)
+            stop += datetime.timedelta(days=1)
         else:
-            print("sleeping until", start)
-            sleep((start - stop).total_seconds())
-            stop + datetime.timedelta(days=1)
+            logger.info("sleeping for %ds until %s", duration, start)
+            sleep(duration)
+            start += datetime.timedelta(days=1)
 
 
 if __name__ == "__main__":
