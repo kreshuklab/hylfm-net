3c0880ed369f2f180595474df143ea05da82fdce
fynnbe <thefynnbe@gmail.com>
2020-05-11 02:00:25 +0200

    cache lr_trf for static validation and test

--- lnet/datasets/heart_static.py
+++ lnet/datasets/heart_static.py
@@ -462,9 +462,9 @@ def check_data(tag: str, comment: str, meta: dict):
     print("get lr")
     lr = get_dataset_from_info(get_tensor_info(tag, "lr", meta=meta))
     print("get ls")
-    ls = get_dataset_from_info(get_tensor_info(tag, "ls", meta=meta), cache=True)
+    ls = get_dataset_from_info(get_tensor_info(tag, "ls", meta=meta), cache=False)
     print("get ls_trf")
-    ls_trf = get_dataset_from_info(get_tensor_info(tag, "ls_trf", meta=meta))
+    ls_trf = get_dataset_from_info(get_tensor_info(tag, "ls_trf", meta=meta), cache=True)
 
     print(tag, len(lf), comment)
     assert len(lf) == len(lr), (tag, len(lf), len(lr))
