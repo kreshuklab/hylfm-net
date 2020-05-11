0819db5b3f935f066ab2d4cb124f53f57a9a0754
fynnbe <thefynnbe@gmail.com>
2020-05-11 03:26:55 +0200

    .

--- scripts/rm_data.py
+++ scripts/rm_data.py
@@ -2,16 +2,47 @@ import shutil
 
 from lnet.settings import settings
 
-root = settings.log_path
+# root = settings.log_path
 
-delete = """
-/g/kreshuk/LF_computed/lnet/logs/beads/z_out51/01highc_f4/20-04-29_16-24-00
-/g/kreshuk/LF_computed/lnet/logs/beads/z_out51/4mu_f4/20-04-29_16-25-14
-/g/kreshuk/LF_computed/lnet/logs/beads/z_out51/01highc_f8/20-04-29_16-24-44
-/g/kreshuk/LF_computed/lnet/logs/beads/z_out51/4mu_f8/20-04-29_16-25-43
-"""
+# delete = """
+# /g/kreshuk/LF_computed/lnet/logs/beads/z_out51/01highc_f4/20-04-29_16-24-00
+# /g/kreshuk/LF_computed/lnet/logs/beads/z_out51/4mu_f4/20-04-29_16-25-14
+# /g/kreshuk/LF_computed/lnet/logs/beads/z_out51/01highc_f8/20-04-29_16-24-44
+# /g/kreshuk/LF_computed/lnet/logs/beads/z_out51/4mu_f8/20-04-29_16-25-43
+# """
 
-for name in delete.strip("\n").split("\n"):
-    shutil.rmtree(root / name)
-    # for file_path in data_path.glob(f"{name}.*"):
-    #     file_path.unlink()
+# for name in delete.strip("\n").split("\n"):
+#     shutil.rmtree(root / name)
+#     # for file_path in data_path.glob(f"{name}.*"):
+#     #     file_path.unlink()
+
+rm_paths = list(settings.cache_path.glob("*mask.npy"))
+for rm_path in rm_paths:
+    print(rm_path)
+    try:
+        shutil.rmtree(rm_path)
+    except NotADirectoryError:
+        rm_path.unlink()
+
+    # txt = txt_path.read_text()
+    # if "index_mask" not in str(txt_path) and "quality" in txt:
+    #     print(txt)
+    #     print(txt_path.with_suffix(""))
+    #     rm_paths = list(settings.cache_path.glob((txt_path.with_suffix("").name + "*")))
+    #     if len(rm_paths) == 1:
+    #         rm_path = rm_paths[0]
+    #         print(rm_path)
+    #
+    #         try:
+    #             shutil.rmtree(rm_path)
+    #         except NotADirectoryError:
+    #             rm_path.unlink()
+    #     else:
+    #         print(rm_paths)
+    #         break
+
+# for name in delete.strip("\n").split("\n"):
+#     txt =
+#     # shutil.rmtree(root / name)
+#     # for file_path in data_path.glob(f"{name}.*"):
+#     #     file_path.unlink()
