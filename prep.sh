1f2ce18d7fd67b92f1317a39b07145c37c0cad93
Fynn Beuttenmueller <fynn.beuttenmueller@embl.de>
2020-05-13 16:56:06 +0200

    seperate prep slurm out

--- prep.sh
+++ prep.sh
@@ -3,8 +3,8 @@
 #SBATCH -n 16                        # number of cores
 #SBATCH --mem 32GB                   # memory pool for all cores
 #SBATCH -t 0-05:01:00                # runtime limit (D-HH:MM:SS)
-#SBATCH -o slurm_out/%N.%j.out       # STDOUT
-#SBATCH -e slurm_out/%N.%j.err       # STDERR
+#SBATCH -o slurm_out_prep/%N.%j.out  # STDOUT
+#SBATCH -e slurm_out_prep/%N.%j.err  # STDERR
 #SBATCH --mail-type=END,FAIL         # notifications for job done & fail
 #SBATCH --mail-user=beuttenm@embl.de # send-to address
 #SBATCH -J heart_dynamic_prep
