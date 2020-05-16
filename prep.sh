d83330c190f176fe8e9b2576e0d02e0519f4ed3c
fynnbe <thefynnbe@gmail.com>
2020-05-16 16:58:33 +0200

    update prep.sh

--- prep.sh
+++ prep.sh
@@ -3,10 +3,10 @@
 #SBATCH -n 8                         # number of cores
 #SBATCH --mem 8GB                    # memory pool for all cores
 #SBATCH -t 0-10:01:00                # runtime limit (D-HH:MM:SS)
-#SBATCH -o /scratch/beuttenm/lnet/slurm_out_prep/%N_%j_%A_%a.out  # STDOUT
-#SBATCH -e /scratch/beuttenm/lnet/slurm_out_prep/%N_%j_%A_%a.err  # STDERR
-#SBATCH --mail-type=END,FAIL         # notifications for job done & fail
-#SBATCH --mail-user=beuttenm@embl.de # send-to address
+#SBATCH -o /scratch/beuttenm/lnet/slurm_out_prep/%N_%A_%a.out  # STDOUT
+#SBATCH -e /scratch/beuttenm/lnet/slurm_out_prep/%N_%A_%a.err  # STDERR
+#SBATCH --mail-type=END,FAIL,ARRAY_TASKS                          # notifications for job done & fail per task
+#SBATCH --mail-user=beuttenm@embl.de                              # send-to address
 #SBATCH -J lnet_prep
 #SBATCH --array=0-0%1
 
