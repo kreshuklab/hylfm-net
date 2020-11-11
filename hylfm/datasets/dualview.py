70e688aa46539f9370c0582da88b0938d13fb8fd
fynnbe <thefynnbe@gmail.com>
2020-11-11 10:02:07 +0100

    add single view datasets from dualview experiment

--- hylfm/datasets/dualview.py
+++ hylfm/datasets/dualview.py
@@ -16,7 +16,6 @@ beads_left = TensorInfo(
     tag="dualbeads_left",
 )
 
-
 heart_right = TensorInfo(
     name="lf",
     root="GKRESHUK",
@@ -25,10 +24,25 @@ heart_right = TensorInfo(
     tag="dualheart_right",
 )
 
-heart_left = TensorInfo(
-    name="lf",
-    root="GKRESHUK",
-    location="LF_computed/LenseLeNet_Microscope/DualView_comparison_heart_movie/heart/Rectified_LC/*.tif",
-    insert_singleton_axes_at=[0, 0],
-    tag="dualheart_left",
-)
+
+def get_tensor_info(tag: str, name: str, meta: dict):
+    root = "GKRESHUK"
+    if tag == "RC_LFD_n156to156_steps4":
+        if name == "lfd":
+            location = "LF_computed/LenseLeNet_Microscope/dualview_060918_added/RC_LFD_-156to156_steps4/Cam_Right_*.tif"
+        elif name == "lf":
+            location = "LF_computed/LenseLeNet_Microscope/dualview_060918_added/RC_rectified/Cam_Right_*.tif"
+        else:
+            raise NotImplementedError(tag, name)
+    elif tag == "LC_LFD_n156to156_steps4":
+        if name == "lfd":
+            location = "LF_computed/LenseLeNet_Microscope/dualview_060918_added/LC_LFD_-156to156_steps4/Cam_Right_*.tif"
+        elif name == "lf":
+            location = "LF_computed/LenseLeNet_Microscope/dualview_060918_added/LC_rectified/Cam_Right_*.tif"
+        else:
+            raise NotImplementedError(tag, name)
+
+    else:
+        raise NotImplementedError(tag, name)
+
+    return TensorInfo(name=name, root=root, location=location, insert_singleton_axes_at=[0, 0], tag=tag)
