d074401c95f0a0a94077761c6ac48beb3f597aba
fynnbe <thefynnbe@gmail.com>
2020-11-18 01:43:45 +0100

    add configs

--- run_hylfm_cpu.sh
+++ run_hylfm_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -N 1                         # number of nodes
 #SBATCH -n 8                         # number of cores
-#SBATCH --mem 24GB                   # memory pool for all cores
+#SBATCH --mem 2GB                   # memory pool for all cores
 #SBATCH -t 6-23:01:00                # runtime limit (D-HH:MM:SS)
 #SBATCH -o /scratch/beuttenm/hylfm/slurm_out/%N.%j.out       # STDOUT
 #SBATCH -e /scratch/beuttenm/hylfm/slurm_out/%N.%j.err       # STDERR
