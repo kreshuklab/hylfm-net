5a28b8dff42a852478c5c7895b223a8222b4a0c7
fynnbe <thefynnbe@gmail.com>
2021-02-09 21:03:21 +0100

    add inspect_slurm_out.py

---
+++ scripts/inspect_slurm_out.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+slurm_out_path = Path("/scratch/beuttenm/hylfm/slurm_out")
+slurm_out_prep_path = Path("/scratch/beuttenm/hylfm/slurm_out_prep")
+
+if __name__ == "__main__":
+    job_id = "11533244".strip()
+    for outpath in slurm_out_path.glob(f"*{job_id}*.out"):
+        print("slurm out")
+        print(outpath.read_text())
+        errpath = outpath.with_suffix(".err")
+        assert errpath.exists(), errpath
+        print("slurm err")
+        print(errpath.read_text())
+
+    for outpath in slurm_out_prep_path.glob(f"*{job_id}*.out"):
+        print("slurm prep out")
+        print(outpath.read_text())
+        errpath = outpath.with_suffix(".err")
+        assert errpath.exists(), errpath
+        print("slurm prep err")
+        print(errpath.read_text())
