29ad717e1f73a2a9841fd6695f9c89b2d00f55ae
fynnbe <thefynnbe@gmail.com>
2021-02-16 20:19:00 +0100

    .

--- hylfm/resume_to_train.py
+++ hylfm/resume_to_train.py
@@ -23,6 +23,7 @@ def resume(
     reset_epoch: Optional[bool] = typer.Option(False, "--reset_epoch"),
     validate_every_unit: Optional[PeriodUnit] = typer.Option(None, "--validate_every_unit"),
     validate_every_value: Optional[int] = typer.Option(None, "--validate_every_value"),
+    eval_batch_size: Optional[int] = typer.Option(None, "--eval_batch_size"),
 ):
     checkpoint = Checkpoint.load(checkpoint)
 
@@ -34,33 +35,37 @@ def resume(
         if impatience is None:
             impatience = 0
 
+    if eval_batch_size is not None:
+        changes["eval_batch_size"] = f"{checkpoint.config.eval_batch_size}->{eval_batch_size}"
+        checkpoint.config.eval_batch_size = eval_batch_size
+
     if best_validation_score is not None:
+        changes["best_validation_score"] = f"{checkpoint.best_validation_score}->{best_validation_score}"
         checkpoint.best_validation_score = best_validation_score
-        changes["best_validation_score"] = best_validation_score
 
     if max_epochs is not None:
+        changes["max_epochs"] = f"{checkpoint.config.max_epochs}->{max_epochs}"
         checkpoint.config.max_epochs = max_epochs
-        changes["max_epochs"] = max_epochs
 
     if reset_epoch:
+        changes["epoch"] = f"{checkpoint.epoch}->0"
         checkpoint.epoch = 0
-        changes["epoch"] = 0
 
     if impatience is not None:
+        changes["impatience"] = f"{checkpoint.impatience}->{impatience}"
         checkpoint.impatience = impatience
-        changes["impatience"] = impatience
 
     if patience is not None:
-        checkpoint.patience = patience
-        changes["patience"] = patience
+        changes["patience"] = f"{checkpoint.config.patience}->{patience}"
+        checkpoint.config.patience = patience
 
     if validate_every_unit is not None:
+        changes["validate_every_unit"] = f"{checkpoint.config.validate_every_unit}->{validate_every_unit}"
         checkpoint.config.validate_every_unit = validate_every_unit
-        changes["validate_every_unit"] = validate_every_unit
 
     if validate_every_value is not None:
+        changes["validate_every_value"] = f"{checkpoint.config.validate_every_value}->{validate_every_value}"
         checkpoint.config.validate_every_value = validate_every_value
-        changes["validate_every_value"] = validate_every_value
 
     if changes:
         notes = "resumed with changes: " + " ".join([f"{k}: {v}" for k, v in changes.items()])
