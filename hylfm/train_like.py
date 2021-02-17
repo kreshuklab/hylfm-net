0a4af22fff1e2b027a52f476f14555a0d73c4b73
fynnbe <thefynnbe@gmail.com>
2021-02-17 20:54:46 +0100

    .

--- hylfm/train_like.py
+++ hylfm/train_like.py
@@ -6,7 +6,14 @@ import typer
 
 from hylfm import __version__
 from hylfm.checkpoint import Checkpoint
-from hylfm.hylfm_types import DatasetChoice, LRSchedThresMode, LRSchedulerChoice, MetricChoice, OptimizerChoice
+from hylfm.hylfm_types import (
+    CriterionChoice,
+    DatasetChoice,
+    LRSchedThresMode,
+    LRSchedulerChoice,
+    MetricChoice,
+    OptimizerChoice,
+)
 from hylfm.train import train
 from hylfm.utils.general import PeriodUnit
 
@@ -19,9 +26,11 @@ logger = logging.getLogger(__name__)
 @app.command()
 def train_model_like(
     model_kwargs_from_checkpoint: Path,
-    dataset: Optional[DatasetChoice] = typer.Option(None, "--dataset"),
     batch_multiplier: Optional[int] = typer.Option(None, "--batch_multiplier"),
     batch_size: Optional[int] = typer.Option(None, "--batch_size"),
+    crit_weight: Optional[float] = typer.Option(None, "--crit_weight"),
+    criterion: Optional[CriterionChoice] = typer.Option(None, "--criterion"),
+    dataset: Optional[DatasetChoice] = typer.Option(None, "--dataset"),
     eval_batch_size: Optional[int] = typer.Option(None, "--eval_batch_size"),
     interpolation_order: Optional[int] = typer.Option(None, "--interpolation_order"),
     lr_sched_factor: Optional[float] = typer.Option(None, "--lr_sched_factor"),
@@ -36,13 +45,13 @@ def train_model_like(
     opt_weight_decay: Optional[float] = typer.Option(None, "--opt_weight_decay"),
     optimizer: Optional[OptimizerChoice] = typer.Option(None, "--optimizer"),
     patience: Optional[int] = typer.Option(None, "--patience"),
+    save_after_validation_iterations: Optional[List[int]] = typer.Option(None, "--save_after_validation_iterations"),
     score_metric: MetricChoice = typer.Option(MetricChoice.MS_SSIM, "--score_metric"),
     seed: Optional[int] = typer.Option(None, "--seed"),
     validate_every_unit: Optional[PeriodUnit] = typer.Option(None, "--validate_every_unit"),
     validate_every_value: Optional[int] = typer.Option(None, "--validate_every_value"),
     win_sigma: Optional[float] = typer.Option(None, "--win_sigma"),
     win_size: Optional[int] = typer.Option(None, "--win_size"),
-    save_after_validation_iterations: Optional[List[int]] = typer.Option(None, "--save_after_validation_iterations"),
     z_out: Optional[int] = typer.Option(None, "--z_out"),
 ):
     reference_checkpoint = Checkpoint.load(model_kwargs_from_checkpoint)
@@ -54,9 +63,11 @@ def train_model_like(
     changes = {
         k: v
         for k, v in {
-            "dataset": dataset,
             "batch_multiplier": batch_multiplier,
             "batch_size": batch_size,
+            "crit_weight": crit_weight,
+            "criterion": criterion,
+            "dataset": dataset,
             "eval_batch_size": eval_batch_size,
             "interpolation_order": interpolation_order,
             "lr_sched_factor": lr_sched_factor,
@@ -71,13 +82,13 @@ def train_model_like(
             "opt_weight_decay": opt_weight_decay,
             "optimizer": optimizer,
             "patience": patience,
+            "save_after_validation_iterations": save_after_validation_iterations,
             "score_metric": score_metric,
             "seed": seed,
             "validate_every_unit": validate_every_unit,
             "validate_every_value": validate_every_value,
             "win_sigma": win_sigma,
             "win_size": win_size,
-            "save_after_validation_iterations": save_after_validation_iterations,
             "z_out": z_out,
         }.items()
         if v is not None
